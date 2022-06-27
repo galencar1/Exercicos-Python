a2dc6cf63223b3cf567274e07a4ae014b3c1f5ac
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 13:55:24 -0300

    MElhorando ex 006

--- ex006.py
+++ ex006.py
@@ -5,8 +5,6 @@ e mostre seu dobro, seu triplo e
 sua raíz quadrada
 """""
 n1 = int(input('Digite um número: '))
-n2 = n1 * 2
-n3 = n1 * 3
-n4 = n1 ** (1 / 2)
-print('O Valor digitado é {}, seu dobro é {}, seu triplo é {}, sua raíz quadrada é {}'.format(n1, n2, n3, n4))
+print('O Valor digitado é {}, \nseu dobro é {}, \nseu triplo é {}, \nsua raíz quadrada é '
+      '{:.2f}'.format(n1, (n1*2), (n1*3), (n1**(1/2))))
 ############################################################################################
