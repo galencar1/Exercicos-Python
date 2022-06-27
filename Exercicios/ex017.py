fc3904dc312098e974c39e081464661b11aaa217
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 17:25:09 -0300

    Resolvendo ex017

--- Exercicios/ex017.py
+++ Exercicios/ex017.py
@@ -1,3 +1,5 @@
+import math
+
 print('='*25)
 print('='*6, 'DESAFIO 017', '='*6)
 """""
@@ -6,3 +8,8 @@ leia o comprimento do cateto oposto
 e do cateto adjacente de um triângulo,
 retângulo, calcule e mostre o comprimento da hipotenusa.
 """""
+print('Catetos e Hipotenusa')
+b = float(input('Digite o valor do cateto oposto: '))
+c = float(input('Digite o valot do cateto adjacente: '))
+print('O valor do cateto oposto é {} e do cateto adjacente é {}, logo a hipotenusa vale {}.'
+      .format(b, c, math.hypot(b, c)))
