52695718b2d00ea796588445210a5d5e3710927b
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 14:01:46 -0300

    OTimizando exercicio 07

--- ex007.py
+++ ex007.py
@@ -5,5 +5,5 @@ Programa calculo de média de alunos
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 n3 = (n1 + n2) / 2
-print('A primeira nota foi {}, a segunda nota foi {}, a média das duas notas é {} '.format(n1, n2, n3))
+print('A primeira nota foi {}, a segunda nota foi {}, a média das duas notas é {:.1f} '.format(n1, n2, ((n1+n2) / 2)))
 ############################################################################################
