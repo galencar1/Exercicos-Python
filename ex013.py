2eb380701ddb6287d70ac388dff91f7859526ae0
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 15:20:55 -0300

    otimizando ex015

--- ex013.py
+++ ex013.py
@@ -4,6 +4,5 @@ Programa lê o salário de um funcionário
 e mostre seu novo salário com 15% de aumento.
 """""
 n1 = float(input('Digite seu salário: '))
-n2 = n1 * 0.15
-n3 = n1 + n2
-print('Seu novo salário será {:.2f}'.format(n3))
+n2 = n1 + (n1 * 15/100)
+print('Seu novo salário será {:.2f}'.format(n2))
