7630d67964b5ccb88b43da0c281f34c83d05bb0d
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 15:15:00 -0300

    Otimizando ex 012

--- ex012.py
+++ ex012.py
@@ -3,8 +3,7 @@ print('======DESAFIO 012======')
 Programa lê o preço do produto 
 e mostre seu novo preço com 5% de desconto.
 """""
-n1 = float(input('Digite o preço do produto: '))
-n2 = n1 * 0.05
-n3 = n1 - n2
-print('O Valor digitado foi {}. Com 5% de desconto passará a ser {:.2f}'.format(n1, n3))
+n1 = float(input('Digite o preço do produto: R$'))
+n2 = n1 - (n1 * 5/100)
+print('O Valor digitado foi {}. Com 5% de desconto passará a ser {:.2f}'.format(n1, n2))
 ############################################################################################
