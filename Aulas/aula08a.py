61211c1456b57db74888e90c621673082809083a
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 16:20:37 -0300

    Aula 08a

---
+++ Aulas/aula08a.py
@@ -0,0 +1,7 @@
+import math
+import emoji
+
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {:.2f}.'.format(num, raiz))
+print(emoji.emojize("Olá Mundo! :earth_americas:", language='alias'))
