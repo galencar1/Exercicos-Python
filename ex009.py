6477c4db909bc8bd39f0bda99d77f5b452208eb9
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 14:33:40 -0300

    Otimizando cex009

--- ex009.py
+++ ex009.py
@@ -2,29 +2,17 @@ print('======DESAFIO 009======')
 """""
 Tabuada
 """""
+print('=' * 12)
 n1 = int(input('Digite um valor: '))
-n2 = n1 * 0
-n3 = n1 * 1
-n4 = n1 * 2
-n5 = n1 * 3
-n6 = n1 * 4
-n7 = n1 * 5
-n8 = n1 * 6
-n9 = n1 * 7
-n10 = n1 * 8
-n11 = n1 * 9
-n12 = n1 * 10
-print('O Valor digitado foi {} . E sua tabuada é '
-      '\n 2 x 0 = {} '
-      '\n 2 x 1 = {} '
-      '\n 2 x 2 = {} '
-      '\n 2 x 3 = {} '
-      '\n 2 x 4 = {} '
-      '\n 2 x 5 = {} '
-      '\n 2 x 6 = {} '
-      '\n 2 x 7 = {} '
-      '\n 2 x 8 = {} '
-      '\n 2 x 9 = {} '
-      '\n 2 x 10 = {} '
-      .format(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12))
+print('{} x {:2} = {}'.format(n1, 1, n1*1))
+print('{} x {:2} = {}'.format(n1, 2, n1*2))
+print('{} x {:2} = {}'.format(n1, 3, n1*3))
+print('{} x {:2} = {}'.format(n1, 4, n1*4))
+print('{} x {:2} = {}'.format(n1, 5, n1*5))
+print('{} x {:2} = {}'.format(n1, 6, n1*6))
+print('{} x {:2} = {}'.format(n1, 7, n1*7))
+print('{} x {:2} = {}'.format(n1, 8, n1*8))
+print('{} x {:2} = {}'.format(n1, 9, n1*9))
+print('{} x {:2} = {}'.format(n1, 10, n1*10))
+print('=' * 12)
 ############################################################################################
