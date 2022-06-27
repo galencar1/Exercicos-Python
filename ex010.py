0a6e24cfeca2ca0dc54d25f254a9b037a366ed8c
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 14:39:40 -0300

    Otimizando ex010

--- ex010.py
+++ ex010.py
@@ -2,9 +2,9 @@ print('======DESAFIO 010======')
 """""
 Programa lê quanto pessoa tem na carteira
 e converte para dólares
-Considere USS 1,00 = R$ 3,27
+Considere USS 1,00 = R$ 5,22
 """""
-n1 = float(input('Digite um valor: '))
-n2 = n1 / 3.27
+n1 = float(input('Quanto dinheiro você tem na carteira? R$'))
+n2 = n1 / 5.22
 print('Você possui R${}. Com esse valor é possível comprar U$${:.2f} .'.format(n1, n2))
 ############################################################################################
