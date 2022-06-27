807cb6446b379603859a39c6b85df531bcff5844
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-27 14:05:55 -0300

    OTimizando ex008

--- ex008.py
+++ ex008.py
@@ -5,7 +5,6 @@ e exibe o resultado convertido em centímetros
 e milímetros.
 """""
 n1 = int(input('Digite um valor: '))
-n2 = n1 * 100
-n3 = n1 * 1000
-print('O Valor digitado foi {} metros. Ele equivale a {} centímetros e a {} milímetros '.format(n1, n2, n3))
+print('O Valor digitado foi {} metros. Ele equivale a {} centímetros e a {} milímetros '
+      .format(n1, (n1*100), (n1*1000)))
 ############################################################################################
