5871452f4c7d346c22139627011cdee2904230d3
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-28 08:24:06 -0300

    Otimizando ex019

--- Exercicios/ex019.py
+++ Exercicios/ex019.py
@@ -1,3 +1,4 @@
+from random import choice
 print('='*25)
 print('='*6, 'DESAFIO 019', '='*6)
 """""
@@ -6,4 +7,10 @@ Faça um programa que ajude ele,
 lendo o nome deles e escrevendo o nome do escolhido.
 """""
 print('Sorteando um item na lista')
-
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quarto nome: '))
+lista = [n1, n2, n3, n4]
+escolhido = choice(lista)
+print('O Aluno escolhido foi {}!'.format(escolhido))
