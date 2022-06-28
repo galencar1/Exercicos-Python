8d1cfddcaf335505619d1b7c51047368eef6222c
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-28 08:49:35 -0300

    Ex021

--- Exercicios/ex021.py
+++ Exercicios/ex021.py
@@ -1,7 +1,12 @@
-print('='*25)
-print('='*6, 'DESAFIO 021', '='*6)
+import pygame
+
 """""
 Faça um programa que abra 
 e reproduza o áudio de um 
 arquivo MP3
 """""
+# Utilizando o módulo Py Game para importar música
+pygame.init()  # Inicia o módulo.
+pygame.mixer.music.load('ex021.mp3')  # carrega a música
+pygame.mixer.music.play()  #Toca a música
+pygame.event.wait()
