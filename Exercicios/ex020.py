90cc27ca0d4ef2de5be7ee082383d90f6613c745
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-28 08:31:02 -0300

    Otimizando ex020

--- Exercicios/ex020.py
+++ Exercicios/ex020.py
@@ -1,3 +1,4 @@
+from random import shuffle
 print('='*25)
 print('='*6, 'DESAFIO 020', '='*6)
 """""
@@ -7,3 +8,10 @@ Faça um programa que leia o nome
 dos quatro alunos e mostre a ordem
 sorteada
 """""
+n1 = str(input('Primeiro nome: '))
+n2 = str(input('Segundo nome: '))
+n3 = str(input('Terceiro nome: '))
+n4 = str(input('Quarto nome: '))
+lista = [n1, n2, n3, n4]
+shuffle(lista)
+print('A Ordem de apresentação será {}'.format(lista))
