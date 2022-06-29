a4d8d2f684f475233008e1a78d37cb103e6856da
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-29 08:25:37 -0300

    Resolvendo ex026

--- Exercicios/ex026.py
+++ Exercicios/ex026.py
@@ -7,3 +7,7 @@ e mostre:
 -> Em que posição ela aparece na primeira vez.
 -> Em que posição aparece a última vez
 '''
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra "a" aparece {} vez(es)'.format(frase.count('A')))
+print('Ela aparece a primeira vez na posição {}'.format(frase.find('A')+1))
+print('Ela aparece pela última vez na posição {}'.format(frase.rfind('A')+1))
