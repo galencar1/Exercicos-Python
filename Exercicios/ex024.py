8523caa801b6d14618399c050ddd06d9949bdb46
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-28 23:26:07 -0300

    Resolvendo ex 024 e 025

--- Exercicios/ex024.py
+++ Exercicios/ex024.py
@@ -6,3 +6,5 @@ que leia o nome de uma
 cidade e diga se ela começa
 ou não com o nome "SANTO"
 '''
+nome = str(input('Digite o nome da cidade: ')).strip()
+print('SANTO' in nome.upper())
--- Exercicios/ex025.py
+++ Exercicios/ex025.py
@@ -5,3 +5,5 @@ Lê o nome de uma pessoa
 e diz se ela tem "Silva"
 no nome.
 '''
+nome = str(input('Qual é o seu nome? ')).strip()
+print('SILVA' in nome.upper())
