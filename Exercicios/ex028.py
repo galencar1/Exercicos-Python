1d80fe0767f9bec108ece0a75f7a981c4c529be6
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-29 11:07:47 -0300

    Resolvendo ex028

--- Exercicios/ex028.py
+++ Exercicios/ex028.py
@@ -1,3 +1,4 @@
+import random
 print('='*25)
 print('='*6, 'DESAFIO 028', '='*6)
 '''
@@ -9,3 +10,14 @@ qual foi o número escolhido pelo PC.
 O programa deverá escrever na tela
 se o usuário vencer ou perdeu.
 '''
+print('-=--'*20)
+print('Vou pensar em um número entre 0 e 5. Tente Adivinhar')
+print('-=--'*20)
+num = [0, 1, 2, 3, 4, 5]
+numr = random.choice(num)
+n1 = int(input('Digite um número de 0 a 5: '))
+if n1 == numr:
+    print('Parabéns! Você acertou!')
+else:
+    print('Não foi dessa vez! Tente novamente.')
+print('O Número que pensei foi {}'.format(numr))
