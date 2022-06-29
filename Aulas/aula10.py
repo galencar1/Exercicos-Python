868e64af8570dfb1f4f85dfe2cfc9539574b7623
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-29 09:54:14 -0300

    Aula

---
+++ Aulas/aula10.py
@@ -0,0 +1,16 @@
+'''
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Zaraki Kenpachi' or nome == 'Yamamoto Genryusai':
+    print('Você é o melhor Capitão de todos!')
+else:
+    print('Você é um Capitão fraco!')
+print('Bom dia {}!'.format(nome))
+'''
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2)/2
+print('A sua média foi {:.1f}'.format(m))
+if m >= 6.0:
+    print('Parabéns!')
+else:
+    print('Estude Mais!')
