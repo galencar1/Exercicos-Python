7b6f362c5eae82bd7ad1a4db35d715d73b1455d4
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-28 23:09:56 -0300

    Resolvendo ex023

--- Exercicios/ex023.py
+++ Exercicios/ex023.py
@@ -11,3 +11,14 @@ Dezena: 3
 Centena: 8
 Milhar: 1
 '''
+num = int(input('Digite um número: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(
+    'Unidade: {}'
+    '\nDezena: {}'
+    '\nCentena: {}'
+    '\nMilhar: {}'.format(u, d, c, m))
+
