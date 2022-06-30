1143f000660019bc36b8e1435dc4ea5e2ea0946c
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-29 22:28:19 -0300

    Resolvendo ex030

--- Exercicios/ex030.py
+++ Exercicios/ex030.py
@@ -5,3 +5,8 @@ Programa que leia um número
 qualquer e mostre na tela
 se ele é par ou ímpar.
 '''
+num = int(input('Digite um número: '))
+if num % 2 == 0:
+    print('O número digitado é par!')
+else:
+    print('O número digitado é impar!')
